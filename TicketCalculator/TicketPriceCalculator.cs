01748e1b3d95b9901a8b63e7431c19b2a4f54886
jnttackie009 <jnttackie009@st.ug.edu.gh>
2024-06-02 22:55:33 +0000

    Commiting the Ticket calculator program

---
+++ TicketCalculator/TicketPriceCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dcit318_assignment1_10982383
+{
+    class TicketPriceCalculator
+    {
+        static void Main()
+        {
+            // Prompt the user to enter their age
+            Console.Write("Please enter your age: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int ticketPrice;
+            if (age <= 12 || age >= 65)
+            {
+                ticketPrice = 7;
+            }
+            else
+            {
+                ticketPrice = 10;
+            }
+
+            // Display the ticket price
+            Console.WriteLine("Your ticket price is: GHC" + ticketPrice);
+        }
+    }
+}
+
